4d65e468ce2e2e5318043d8bb440e81781e3b031
kolodei s.v. <you@example.com>
2025-01-18 17:09:51 +0300

    starts

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,12 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) throws SQLException {
+        DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres", "mugivara25");
     }
 }
